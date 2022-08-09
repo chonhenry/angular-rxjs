9a5558a1b9d934ad9ac0e50a6f119e37692c55f9
chonhenry <chonhenry@gmail.com>
2022-08-08 22:32:50 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-rxjs';
+export class AppComponent implements OnInit {
+  title = 'AngularObservables';
+
+  myObservable = new Observable((observer) => {
+    console.log('Observable starts');
+    setTimeout(() => {
+      observer.next('1');
+    }, 1000);
+    setTimeout(() => {
+      observer.next('2');
+    }, 2000);
+    setTimeout(() => {
+      observer.next('3');
+    }, 3000);
+    setTimeout(() => {
+      observer.error(new Error('Something went wrong'));
+    }, 3000);
+    setTimeout(() => {
+      observer.next('4');
+    }, 4000);
+    setTimeout(() => {
+      observer.next('5');
+    }, 5000);
+    setTimeout(() => {
+      observer.complete();
+    }, 3000);
+  });
+
+  ngOnInit() {
+    this.myObservable.subscribe({
+      next: (val) => console.log(val),
+      error: (err) => console.error(err.message),
+      complete: () => console.log('Observable complete'),
+    });
+  }
 }
