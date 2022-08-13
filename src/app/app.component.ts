8c7816ca99f994fab5aa216f12d044940ba276eb
chonhenry <chonhenry@gmail.com>
2022-08-13 19:42:17 -0400

    unsubscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,41 +14,35 @@ export class AppComponent implements OnInit {
 
   constructor(private dataService: DataService) {}
 
-  // myObservable = new Observable((observer) => {
-  //   console.log('Observable starts');
-  //   setTimeout(() => {
-  //     observer.next('1');
-  //   }, 1000);
-  //   setTimeout(() => {
-  //     observer.next('2');
-  //   }, 2000);
-  //   setTimeout(() => {
-  //     observer.next('3');
-  //   }, 3000);
-  //   setTimeout(() => {
-  //     observer.error(new Error('Something went wrong'));
-  //   }, 3000);
-  //   setTimeout(() => {
-  //     observer.next('4');
-  //   }, 4000);
-  //   setTimeout(() => {
-  //     observer.next('5');
-  //   }, 5000);
-  //   setTimeout(() => {
-  //     observer.complete();
-  //   }, 3000);
-  // });
+  myObservable = new Observable((observer) => {
+    console.log('Observable starts');
+    setTimeout(() => {
+      observer.next('1');
+    }, 1000);
+    setTimeout(() => {
+      observer.next('2');
+    }, 2000);
+    setTimeout(() => {
+      observer.next('3');
+    }, 3000);
+    setTimeout(() => {
+      observer.error(new Error('Something went wrong'));
+    }, 6000);
+    // setTimeout(() => {
+    //   observer.complete();
+    // }, 3000);
+  });
 
   array1 = [1, 2, 6, 7, 8];
 
-  myObservable = from(this.array1).pipe(
-    map((val) => {
-      return val * 5;
-    }),
-    filter((val) => {
-      return val >= 30;
-    })
-  );
+  // myObservable = from(this.array1).pipe(
+  //   map((val) => {
+  //     return val * 5;
+  //   }),
+  //   filter((val) => {
+  //     return val >= 30;
+  //   })
+  // );
 
   randomNumObs = new Observable((observer) => {
     observer.next(Math.random());
@@ -57,11 +51,15 @@ export class AppComponent implements OnInit {
   randomNumSubject = new Subject();
 
   ngOnInit() {
-    // this.myObservable.subscribe({
-    //   next: (val) => console.log(val),
-    //   error: (err) => console.error(err.message),
-    //   complete: () => console.log('Observable complete'),
-    // });
+    let subscription = this.myObservable.subscribe({
+      next: (val) => console.log(val),
+      error: (err) => console.error(err.message),
+      complete: () => console.log('Observable complete'),
+    });
+
+    setTimeout(() => {
+      subscription.unsubscribe();
+    }, 5000);
 
     this.randomNumSubject.subscribe({
       next: (val) => console.log('subscription 1:', val),
